0405ca76fc65c3c15e18157a9b027c544075bc5d
Nicholas Gust <nicholasgust@gmail.com>
2016-01-23 12:35:01 -0800

    fixed url, in show messages

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160123010507) do
+ActiveRecord::Schema.define(version: 2016012312300000) do
 
   create_table "messages", force: :cascade do |t|
     t.string "title"
     t.string "author"
     t.string "content"
+    t.string "url"
   end
 
 end
