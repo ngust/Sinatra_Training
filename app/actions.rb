f8dad421868627e6c72dfc6bf9d46ebce4d3951c
Nicholas Gust <nicholasgust@gmail.com>
2016-01-23 12:10:33 -0800

    posts stuff working

--- app/actions.rb
+++ app/actions.rb
@@ -12,3 +12,21 @@ get '/messages/new' do
   #@message = Message.new
   erb :'messages/new'
 end
+
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'messages/show'
+end
+
+post '/messages' do
+  @message = Message.new(
+    title: params[:title],
+    content: params[:content],
+    author:  params[:author],
+  )
+  if @message.save
+    redirect '/messages'
+  else
+    erb :'messages/new'
+  end
+end
